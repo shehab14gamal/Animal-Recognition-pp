aa6ba19e80169c7ce4597881ca308e6ff579b748
shehab <shehab@shehabs-Mac.local>
2019-10-04 10:31:46 +0200

    Initial Commit

---
+++ animalsRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animalsRecognition
+//
+//  Created by shehab on 10/4/19.
+//  Copyright © 2019 shehab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
