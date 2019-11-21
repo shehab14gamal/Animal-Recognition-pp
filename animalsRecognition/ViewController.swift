2826f0d2fa6032242597f7d12ab648263eddc0c2
shehab <shehab@shehabs-Mac.local>
2019-11-21 17:22:28 +0200

    complete app

--- animalsRecognition/ViewController.swift
+++ animalsRecognition/ViewController.swift
@@ -7,14 +7,101 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Social
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate , UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        if  let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+        
+        imageView.image = pickedImage
+            
+            
+            guard let ciImage = CIImage(image : pickedImage) else{
+            fatalError("failed to convert into ciImage")
+            }
+            
+            
+            detect(image: ciImage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
     }
 
-
+    func detect(image : CIImage)  {
+        guard let model = try? VNCoreMLModel(for: ImageClassifier().model)else {
+            fatalError("failed to create model")
+        }
+        
+        
+        
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                guard   let result = request.results as? [VNClassificationObservation]else{
+                    fatalError("failed to get results")
+                }
+                
+                
+                if let firstResult = result.first{
+                    if firstResult.identifier.contains("nose"){
+                        self.navigationItem.title = "nose"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                    }else if firstResult.identifier.contains("eye"){
+                        self.navigationItem.title = "eye"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                    }else if firstResult.identifier.contains("ear"){
+                        self.navigationItem.title = "ear"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                    }else{
+                        self.navigationItem.title = "Unrecognizable"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                    }
+                    
+                    
+                }
+                
+        }
+        
+        let handler = VNImageRequestHandler(ciImage : image)
+        try! handler.perform([request])
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
